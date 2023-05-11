3003747d295e6c4a4f0f3859f6df7c1550b315a3
Georly Bibas <denverparole@gmail.com>
2023-05-11 10:30:22 +0800

    attempt to use local css with bootstrap

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask (__name__)
+app.config['SECRET_KEY'] = "moonlight"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a Route Decoratoe
 @app.route('/')
@@ -39,3 +47,14 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name', methods = ['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name == form.name.data
+        form.name.data = ''
+    return render_template("name.html", name = name, form = form)
