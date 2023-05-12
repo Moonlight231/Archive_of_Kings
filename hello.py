343804f821343512767b9f1c828fa1ee77bf3003
Georly Bibas <denverparole@gmail.com>
2023-05-12 14:18:44 +0800

    created and linked database(SQLite and MySQL)

--- hello.py
+++ hello.py
@@ -1,19 +1,72 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from os import path
+
+
 
 # Create a Flask Instance
 app = Flask (__name__)
-app.config['SECRET_KEY'] = "moonlight"
 
+# Secret Key!
+app.config['SECRET_KEY'] = "moonlight"
+# Add Database
+# OLD SQLite db
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# NEW MySQL DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/our_users'
+
+# Initialize the Database
+db =  SQLAlchemy(app)
+
+
+
+
+#Create Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # Create a String
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+# Create a User Class
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# Update Database Record
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        try:
+            db.session.commit()
+            flash("User Updated Successfully!")
+            return render_template("update.html", form=form, name_to_update=name_to_update)
+        except:
+            flash("Error! Looks like there was a problem... Try Again.")
+            return render_template("update.html", form=form, name_to_update=name_to_update)
+    else:
+        return render_template("update.html", form=form, name_to_update=name_to_update)
+    
 # Create a Form Class
 class NamerForm(FlaskForm):
     name = StringField("What's Your Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
-# Create a Route Decoratoe
-@app.route('/')
+
 
 #def index():
 #    return "<h1>Hello World!</h1>"
@@ -27,6 +80,11 @@ class NamerForm(FlaskForm):
 #trim
 #striptags
 
+
+
+
+# Create a Route Decorator
+@app.route('/')
 def index():
     first_name = "John"
     return render_template("index.html",first_name=first_name)
@@ -34,7 +92,9 @@ def index():
 # localhost:5000/user/john
 @app.route('/user/<name>')
 def user(name):
-    return render_template("user.html", name=name)
+    return render_template("user.html", user_name=name)
+
+
 
 # Create Custom Error Pages
 
@@ -55,6 +115,24 @@ def name():
     form = NamerForm()
     # Validate Form
     if form.validate_on_submit():
-        name == form.name.data
+        name = form.name.data
         form.name.data = ''
+        flash("Form Submitted Successfully!")
     return render_template("name.html", name = name, form = form)
+
+@app.route('/user/add', methods = ['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user =  Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User Added Successfully!")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form=form, name=name, our_users=our_users)
